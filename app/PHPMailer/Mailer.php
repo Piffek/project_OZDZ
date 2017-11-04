693a6e164458e0a85e7eab69ef50d86c53224db5
Piffek <patrykpiwko123412@gmail.com>
2017-11-04 16:18:20 +0100

    implement PHPMailer

---
+++ app/PHPMailer/Mailer.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\PHPMailer;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mailer
+{
+    const SMTPDebug = 2;
+    const SMTPSecure = 'tls';
+    const PORT = 2525;
+    
+    protected $mail, $from, $to, $subject, $body, $smtpDebug, $host, $smtpAuth, $username, $password;
+    
+    /**
+     * 
+     * @param array  $from     ['mail', 'name']
+     * @param array  $to       ['mail', 'name']
+     * @param string $body     body of message
+     * @param string $subject  subject of message
+     */
+    public function __construct(
+        array $from,
+        array $to,
+        string $body,
+        string $subject
+    )
+    {
+        $this->mail = new PHPMailer(true);
+        $this->host = getenv('MAIL_HOST');
+        $this->smtpAuth = getenv('SMTPAuth');
+        $this->username = getenv('MAIL_USERNAME');
+        $this->password = getenv('MAIL_PASSWORD');
+        $this->from = $from;
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+    
+    protected function configuration()
+    {
+        $this->mail->SMTPDebug = self::SMTPDebug;
+        $this->mail->isSMTP();
+        $this->mail->Host = $this->host;
+        $this->mail->SMTPAuth = $this->smtpAuth;
+        $this->mail->Username = $this->username;
+        $this->mail->Password = $this->password;
+        $this->mail->SMTPSecure = self::SMTPSecure;
+        $this->mail->Port = self::PORT;
+    }
+    
+    public function recipients()
+    {
+        $this->mail->setFrom($this->from[0], $this->from[1]);
+        $this->mail->addAddress($this->to[0],  $this->to[1]);
+        $this->mail->addReplyTo('info@example.com', 'Information');
+        $this->mail->addCC('cc@example.com');
+        $this->mail->addBCC('bcc@example.com');
+    }
+    
+    public function attachments(string $file)
+    {
+        if(!is_null($file)){
+            $this->mail->addAttachment('/var/tmp/file.tar.gz');
+        }
+    }
+    
+    public function content()
+    {
+        $this->mail->isHTML(true);
+        $this->mail->Subject = $this->subject;
+        $this->mail->Body    = $this->body;
+    }
+    
+    public function mail()
+    {                          
+        try {
+            $this->configuration();
+            $this->recipients();
+            $this->content();
+            $this->mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $this->mail->ErrorInfo;
+        }
+    }
+}
--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace Src\Controllers;
 
 use Src\Models\User;
 use App\Controller;
+use App\PHPMailer\Mailer;
 
 class IndexController extends Controller
 {
@@ -15,7 +16,8 @@ class IndexController extends Controller
         $user = new User();
         $session = isset($_SESSION['username']) ? $_SESSION['username'] : null;
         $loginUrl = $this->getService('FacebookService')->loginurl;
-        
+        //$mailer = new Mailer(['patrykpiwko123412@gmail.com', 'Patryk'], ['patryk123412@op.pl', 'Patryk2'], 'cos', 'cossubject');
+        //$mailer->mail();
         echo $this->render(
             'index.html.twig', array(
             'user' => $user->getAll('user'),
