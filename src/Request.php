7a114758d2af7c6882840ce28685a5ddb64bc395
Piffek <patrykpiwko123412@gmail.com>
2017-12-08 20:16:02 +0100

    ref

--- src/Request.php
+++ src/Request.php
@@ -4,51 +4,41 @@ namespace Src;
 
 class Request
 {
-    public $param = array();
-    public static $paramOfGet = array();
-    
+    public $param = [];
+
+    public static $paramOfGet = [];
+
     /**
      * Get first param from URL.
-     * 
+     *
      * @return string
      */
     public static function getFirstPartOfUrl()
     {
-        $url =  trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+        $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
         $arrWithUrl = explode('/', str_replace(['?'], ['/'], $url));
+
         return $arrWithUrl[0];
     }
-    
+
     /**
      * Group url to metod and value.
-     * 
+     *
      * @return array
      */
     public static function groupURLToKeyAndValueAvailableInControllers()
     {
         $path = trim($_SERVER['REQUEST_URI'], '/');
 
-        @list($param) = explode('/',  str_replace(['?'], ['/'], $path), 1);
+        @list($param) = explode('/', str_replace(['?'], ['/'], $path), 1);
 
-        if($param) {
-            $equal = strpos($param, '=');
-            if($equal){
-                $paramReplace = str_replace(['?'], ['/'], $param);
+        if ($param) {
 
-                $paramEqual =  explode('=', $paramReplace);
+            self::ifGetHasEqual($param);
 
-                $methodAndParam = explode('/', $paramEqual[0]);
-
-
-                array_push($methodAndParam, $paramEqual[1]);
-
-
-                array_push(self::$paramOfGet, $methodAndParam);
-            }
-
-            if(!empty(self::$paramOfGet)){
+            if (! empty(self::$paramOfGet)) {
                 $param = self::$paramOfGet[0];
-            }else{
+            } else {
                 $param = explode('/', $path);
             }
 
@@ -56,24 +46,40 @@ class Request
 
             unset($param[$key]);
 
-            $parameters = array();
+            $parameters = [];
 
-            foreach(array_chunk($param, 2) as $met){
+            foreach (array_chunk($param, 2) as $met) {
                 $parameters[$met[0]] = $met[1];
-                
             }
+
             return $parameters;
         }
     }
-    
+
+
+    public static function ifGetHasEqual($param)
+    {
+        $equal = strpos($param, '=');
+        if ($equal) {
+            $paramReplace = str_replace(['?'], ['/'], $param);
+
+            $paramEqual = explode('=', $paramReplace);
+
+            $methodAndParam = explode('/', $paramEqual[0]);
+
+            array_push($methodAndParam, $paramEqual[1]);
+
+            array_push(self::$paramOfGet, $methodAndParam);
+        }
+    }
+
     /**
      * Get method from URL.
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public static function getUrlMethod()
     {
         return $_SERVER['REQUEST_METHOD'];
-    
     }
 }
