f6a1e18534b45a3151d1fc34e02955261c7acff8
Marcin Lubryka <lubryka.marcin@gmail.com>
2018-02-16 18:09:10 +0100

    formatowanie otrzymywanych maili

--- app/Helpers/SendMailToAllUser.php
+++ app/Helpers/SendMailToAllUser.php
@@ -7,52 +7,64 @@ use Src\PHPMailer\Mailer;
 
 class SendMailToAllUser
 {
-    
+
     public function send(Category $category)
     {
-        
+
         $name = $category->getAll('category');
-        
-        foreach($name as $row){
+
+        foreach ($name as $row) {
             $mailer = new Mailer(
                 [$row->user_email, 'Patryk'],
                 [$row->user_email, $row->user_email],
                 $this->createBody($row->name),
                 'cossubject');
-                $mailer->mail();
+            $mailer->mail();
         }
     }
-    
+
     public function createBody($category)
     {
-        if($category === 'sport'){
+        if ($category === 'sport') {
             $news = file_get_contents('http://irollup.co.uk/project/getApiSport.php/?pass=415d7fe0d9f4fff5977c50d786ca5b9871a7e778');
-        }else if($category === 'politic'){
+        } else if ($category === 'politic') {
             $news = file_get_contents('http://irollup.co.uk/project/getApi.php/?pass=415d7fe0d9f4fff5977c50d786ca5b9871a7e778');
-        }else if($category === 'music'){
+        } else if ($category === 'music') {
             $news = file_get_contents('http://irollup.co.uk/project/getApiMusic.php/?pass=415d7fe0d9f4fff5977c50d786ca5b9871a7e778');
-        }else if($category === 'anything'){
+        } else if ($category === 'anything') {
             $news = file_get_contents('http://irollup.co.uk/project/getApiWeather.php/?pass=415d7fe0d9f4fff5977c50d786ca5b9871a7e778');
         }
-        
+
         $dec = json_decode($news);
-        $t = "";
-        
-        foreach($dec as $d){
-            $t .= $d->title;
+        $tresc = "";
+
+        foreach ($dec as $d) {
+            $tresc .= '</br><a target="_blank" href="' . $d->link . '">' .
+                '<div class="tile">' .
+                '<table>' .
+                '<col class="imgCol">' .
+                '<col class="contentCol">' .
+                '<tr><td rowspan="5"><div class="img"><img src="' . $d->img . '"/></div></td></tr>' .
+                '<tr><td><div class="title">' . $d->title . '</div></td></tr>' .
+                '<tr><td><div class="description">' . $d->description . '</div></td></tr>' .
+                '<tr><td><div class="link">' . $d->link . '</div></td></tr>' .
+                '<tr><td><div class="date">' . $d->date . '</div></td></tr>' .
+                '</table></div></a>';
+
+            $tresc .= $d->title;
         }
-        
+
         return '  <!DOCTYPE html>
                     <html>
                     <body>
             
-                    <h1>'.$t.'</h1>
-                        
-                    <p><b>My first paragraph.</b></p>
+                    <h3>Twoje nowe wiadomości:</h3>
+                       ' . $tresc . ' 
+                    <p><b>Jeśli chcesz przerwać subskrypcję, skontaktuj się z administratorem strony.</b></p>
                         
                     </body>
                     </html>
             ';
-        
+
     }
 }
